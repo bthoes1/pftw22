059b141deb6363ff80ea0a31189d7cb92a2238d4
Brian Thoes <bthoes@mcad.edu>
2022-01-26 21:51:32 -0600

    added prompt for favMovie

--- week2/exercises/ex1/arrays.js
+++ week2/exercises/ex1/arrays.js
@@ -1,9 +1,4 @@
 let myMovies = ['The Corpse Bride', 'The Dark Crystal', 'Bridesmaids', 'The Queen', 'Spirited Away'];
-console.log(myMovies);
-
-myMovies.push('Parasite');
-console.log(myMovies);
-
 let yourMovie = prompt("Enter your favorite movie.");
 myMovies.push(yourMovie);
-console.log(myMovies);
+console.log('Your movie has been added to the list!', myMovies.toString());
