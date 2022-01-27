f702847dbaead1c9f36c236a7ff021df9b5b4781
Brian Thoes <bthoes@mcad.edu>
2022-01-26 21:24:40 -0600

    week2/ex1 first commit

---
+++ week2/exercises/ex1/arrays.js
@@ -0,0 +1,9 @@
+let myMovies = ['The Corpse Bride', 'The Dark Crystal', 'Bridesmaids', 'The Queen', 'Spirited Away'];
+console.log(myMovies);
+
+myMovies.push('Parasite');
+console.log(myMovies);
+
+let yourMovie = prompt("Enter your favorite movie.");
+myMovies.push(yourMovie);
+console.log(myMovies);
