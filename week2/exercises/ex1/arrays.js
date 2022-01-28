629707893852e2413064b6d40daf6e44f272740f
Brian Thoes <bthoes@mcad.edu>
2022-01-27 21:06:44 -0600

    added alert, concats

--- week2/exercises/ex1/arrays.js
+++ week2/exercises/ex1/arrays.js
@@ -1,4 +1,9 @@
-let myMovies = ['The Corpse Bride', 'The Dark Crystal', 'Bridesmaids', 'The Queen', 'Spirited Away'];
-let yourMovie = prompt("Enter your favorite movie.");
-myMovies.push(yourMovie);
+let myMovies = [' The Corpse Bride', ' The Dark Crystal', ' Bridesmaids', ' The Queen', ' Spirited Away'];
+console.log('My favorite movies are', myMovies);
+
+let yourMovie = window.prompt("Enter your favorite movie.");
+myMovies.push(' ' + yourMovie);
+
 console.log('Your movie has been added to the list!', myMovies.toString());
+
+window.alert('Here is our list of favorite movies: ' + myMovies.toString());
