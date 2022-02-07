e193b50d643f8fbebbbeee6ecbddeceeb84461a6
Brian Thoes <bthoes@mcad.edu>
2022-02-06 19:49:05 -0500

    Update radial.js

--- week3/assignment/radial.js
+++ week3/assignment/radial.js
@@ -1,28 +1,43 @@
+// flashing lights warning
+const warning = window.alert('Please be aware that the following contains rapidly changing colors in a strobe effect.');
+console.log(warning);
+
+// starting value directions
 let rotateBy = 50;
 let originX = 150;
 let originY = 150;
+let r, g, b;
+let lineWeight = 2;
 
 function setup() {
     createCanvas(600, 600);
-    background('#152238');
+    background('black');
     angleMode(DEGREES);
+    r = random(255);
+    g = random(255);
+    b = random(255);
 }
 
-function makeArm(rotateBy, originX, originY) {
+function makeArm(rotateBy, originX, lineWeight) {
     let alt= Math.round(rotateBy / 360);
     console.log(alt);
+    // ellipse properties
     noFill();
-    stroke('yellow');
-    strokeWeight(1);
-    ellipse(originX / alt, originY - alt, 150 / alt, 10 * alt);
+    stroke(r, g, b);
+    strokeWeight(lineWeight);
+    ellipse(originX / alt, originY - alt, 150 / alt, 10 + alt);
 }
 
 function draw() {
     translate(300, 300);
     rotate(rotateBy);
-    makeArm(rotateBy, originX, originY);
-    rotateBy += 20;
-    originX += 10;
+    makeArm(rotateBy, originX, lineWeight);
+    rotateBy += 20; // adds 20 to rotation value for each new ellipse
+    originX -= 10; // subtracts 10 from originX value of each new ellipse
+    lineWeight -= .001; // descreases stroke weight of each new ellipse
+    r = random(255);
+    g = random(255);
+    b = random(255);
 }
 
 function mousePressed() {
