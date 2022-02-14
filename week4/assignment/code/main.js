ca8e703031252e8749257310a8db87a772e5f637
Brian Thoes <bthoes@mcad.edu>
2022-02-13 23:12:35 -0600

    adjusted colors

--- week4/assignment/code/main.js
+++ week4/assignment/code/main.js
@@ -7,9 +7,10 @@ let startingId = 0;
 
 function setup () {
     createCanvas(1100, 750);
-    background(0);
+    background('#323232');
     for (let k = 0; k < 3; k++) {
         for (let i = 0; i < 4; i++) {
+            fill(0);
             rect(startingX, startingY, rectWidth, rectHeight);
             myCards.push({ x: startingX, y: startingY, id: startingId });
             startingX += 225;
