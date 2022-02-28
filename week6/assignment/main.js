9af747bc9a72ac217f993869687cf6f26c97bf6c
Brian Thoes <bthoes@mcad.edu>
2022-02-27 20:10:33 -0600

    commented

--- week6/assignment/main.js
+++ week6/assignment/main.js
@@ -1,19 +1,32 @@
 new Vue({
     el: "#myApp",
     data: {
-      factOne: "Baby llamas are called \"Crias.\" Crias can walk within their first hour of life\!",
-      factTwo: "Each llama foot has only two toenails.",
-      factThree: "Llamas are believed to have first appeared in North America, but migrated to South America slowly more than 3 million years ago.",
-      factFour: "Llamas can migrate up and down mountains depending on temperature and season.",
-      factFive: "The llama is the national symbol of Peru.",
-      factSix: "Llamas spit when angry. It is thought that this helps them avoid the need for physical aggression.",
-      factSeven: "Llamas use a humming and gurgling to vocally communicate. These gurgles are referred to as \"orgles.\"",
-      factEight: "Llama antibodies are being used in trials aimed at developing universal flu vaccines.",
-      factNine: "Llamas can travel up to 20 miles per day, making them useful as pack animals.",
-      factTen: "Llamas are not the same as alpacas.",
-      factEleven: "Because of the soft pads of their feet, llamas have very low impact on the ground.",
-      message: "Redacted. Check the code to see how this works."
-
+        
+        // list of llama facts
+        factOne: "Baby llamas are called \"Crias.\" Crias can walk within their first hour of life\!",
+      
+        factTwo: "Each llama foot has only two toenails.",
+      
+        factThree: "Llamas are believed to have first appeared in North America, but migrated to South America slowly more than 3 million years ago.",
+      
+        factFour: "Llamas can migrate up and down mountains depending on temperature and season.",
+      
+        factFive: "The llama is the national symbol of Peru.",
+      
+        factSix: "Llamas spit when angry. It is thought that this helps them avoid the need for physical aggression.",
+      
+        factSeven: "Llamas use a humming and gurgling to vocally communicate. These gurgles are referred to as \"orgles.\"",
+      
+        factEight: "Llama antibodies are being used in trials aimed at developing universal flu vaccines.",
+      
+        factNine: "Llamas can travel up to 20 miles per day, making them useful as pack animals.",
+      
+        factTen: "Llamas are not the same as alpacas.",
+      
+        factEleven: "Because of the soft pads of their feet, llamas have very low impact on the ground.",
+      
+        // special message to appear as v-else
+        message: "Redacted. Check the code to see how this works."
     }
   });
   
