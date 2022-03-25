3c8367495758170bc3341dae998d049630da57a3
Brian Thoes <bthoes@mcad.edu>
2022-03-24 20:56:59 -0500

    9.2

---
+++ week9/exercise2/src/components/CamelidRow.vue
@@ -0,0 +1,32 @@
+<script setup>
+    const props = defineProps(["animal"]);
+    console.log(props);
+</script>
+
+<template>
+    <div>
+        <h3>{{props.animal.commonName}}</h3>
+        <p>Binomial Name: {{props.animal.binomialName}}</p>
+        <p>Family: {{props.animal.family}}</p>
+        <p>Genus: {{props.animal.genus}}</p>
+        <p>Species: {{props.animal.species}}</p>
+        <img v-bind:src="props.animal.image" v-bind:alt="props.animal.commonName">
+    </div>
+</template>
+
+<style scoped>
+    div {
+        max-width: 200px;
+        border: 1px solid black;
+        border-radius: 4px;
+        padding: 10px;
+        margin: 10px;
+    }
+
+    img {
+        max-width: 200px;
+        display: block;
+
+    }
+
+</style>
