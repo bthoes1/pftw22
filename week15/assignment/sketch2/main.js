a6844a12db2c3ea502ded7dec15ecdaf27967926
Brian Thoes <bthoes@mcad.edu>
2022-05-07 15:34:32 -0500

    added interactivity

--- week15/assignment/sketch2/main.js
+++ week15/assignment/sketch2/main.js
@@ -3,17 +3,13 @@ let slider;
 
 function setup() {
     createCanvas(800,800);
-    slider = createSlider(0,255,100);
-    slider.position(10,10);
-    slider.style('width', '80px');
-    for (var i = 0; i < 2000; i++) {
+    for (var i = 0; i <2000; i++) {
         swarm[i] = new Particle();
     }
 }
 
 function draw() {
     background(50);
-    let val = slider.value();
     for (var i = 0; i < swarm.length; i++) {
         swarm[i].update();
         swarm[i].display();
@@ -24,12 +20,12 @@ function Particle() {
     this.position = new createVector(width/2, height/2);
     this.velocity = new createVector();
     this.acceleration = new createVector();
-    this.topspeed = 20;
+    this.topspeed = 5;
 
   this.update = function() {
-    var point = createVector(random(0,width),random(0,height));
+    var point = createVector(mouseX,random(0,height));
     this.acceleration = p5.Vector.sub(point,this.position);
-    this.acceleration.setMag(0.05);
+    this.acceleration.setMag(0.1);
 
     this.velocity.add(this.acceleration);
     this.velocity.limit(this.topspeed);
@@ -39,6 +35,6 @@ function Particle() {
   this.display = function() {
     noFill();
     stroke('pink');
-    ellipse(this.position.x, this.position.y,5,5);
+    ellipse(this.position.x, this.position.y,1,1);
   };
 }
