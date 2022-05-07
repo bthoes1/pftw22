df3b06c855905254e4b56b19bbc79bb58387a042
Brian Thoes <bthoes@mcad.edu>
2022-05-07 16:30:52 -0500

    sketch 2

--- week15/assignment/sketch1/main.js
+++ week15/assignment/sketch1/main.js
@@ -1,14 +1,35 @@
+let slider1;
+let slider2;
+var inc = .005;
+
 function setup() {
-    createCanvas(600, 600);
-    background(50);
+    createCanvas(800,800);
+    slider1 = createSlider(0,0.025,0.0025,0.000001);
+    slider1.position(50, 900);
+    slider1.style('width', '800px');
+
+    slider2 = createSlider(0,0.025,0.0025,0.000001);
+    slider2.position(475, 475);
+    slider2.style('width', '800px');
+    slider2.style('transform', 'rotate(90deg)');
 }
 
 function draw() {
-    for (var i = 200; i <= 400; i += 200) {
-        for (var j = 200; j <= 400; j += 100) {
-            stroke('#E0707C');
-            strokeWeight(.1);
-            line(i + 5, j, mouseX, mouseY);
+    var val1 = slider1.value();
+    var val2 = slider2.value();
+    var yoff = 0;
+    loadPixels();
+    for (var y = 0; y < height; y++) {
+        var xoff = 0;
+        for (var x = 0; x < width; x++) {
+            var index = (x + y * width) * 4;
+            pixels[index + 0] = noise(xoff, yoff) * 224;
+            pixels[index + 1] = noise(xoff, yoff) * 112;
+            pixels[index + 2] = noise(xoff, yoff) * 124;
+            pixels[index + 3] = 255;
+            xoff += val1;
         }
+        yoff += val2;
     }
+    updatePixels();
 }
